8bfdea77a12783f9d0a6cdb497a6c1f3a32f7278
kirill <kirimihi@rambler.ru>
2018-09-21 21:09:59 +0300

    fcm sender with okhttp

--- app/src/main/java/by/home/dartlen/dindindon/MainActivity.kt
+++ app/src/main/java/by/home/dartlen/dindindon/MainActivity.kt
@@ -2,11 +2,18 @@ package by.home.dartlen.dindindon
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
+import java.sql.Timestamp
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var fcm = FCMSender()
+        var users = JSONArray()
+        var time = Timestamp(100002)
+        fcm.FcmSend(users, "Title", "Message", time)
     }
 }
